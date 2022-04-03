4ef44f92d126a0f8bc99d9de8e7324317e099492
Thomas Gaenzle <thgait00@hs-esslingen.de>
2022-04-04 00:09:35 +0200

    updated init function - still need to check turnOn off the LEDs

--- Lights_ESP8266_V1.ino
+++ Lights_ESP8266_V1.ino
@@ -17,33 +17,21 @@
  *  - 300 - 500 Ohm resistor between D1 output and first pixel's data input
  * Minimize distance between Arduino and first pixel.  
  * Avoid connecting on a live circuit...if you must, connect GND first.
- *************************************************************************************************************
- *
- * Pull Up resistors for all input pins! -> Needed with port expander MCP23017
- *
- *************************************************************************************************************/
+ ************************************************************************************************************/
 
 #include <Adafruit_NeoPixel.h>  // for controlling the LEDs
-#include <MCP23017.h>        // for controlling MCP23017
+#include <MCP23017.h>           // for controlling MCP23017
 #include <Wire.h>               // for controlling I2C bus
 #include "glasses.h"
 
 // pin definition
-#define LEDDataLeft     5   // data output for LEDs on left side of board   (D1 Output on D1 Mini from AzDelivery)
-#define LEDDataRight    4   // data output for LEDs on right side of board  (D2 Output on D1 Mini from AzDelivery)
+/* 5 (D1) -> SCL and 4 (D2) -> SDA are reserved as I2C pins */
+#define LEDDataLeft     13  // data output for LEDs on left side of board   (D7 Output on D1 Mini from AzDelivery)
+#define LEDDataRight    15  // data output for LEDs on right side of board  (D8 Output on D1 Mini from AzDelivery)
 #define INTERRUPT_LEFT  14  // interrupt pin for left MCP (D5 Output on D1 Mini from AzDelivery)
 #define INTERRUPT_RIGHT 12  // interrupt pin for right MCP (D6 Output on D1 Mini from AzDelivery)
-
-/*#define reedGlass1  1   // reed below glass 1   (TX Output on D1 Mini from AzDelivery)
-#define reedGlass2  3   // reed below glass 2   (RX Output on D1 Mini from AzDelivery)
-#define reedGlass3  5   // reed below glass 3   (D1 Output on D1 Mini from AzDelivery)
-#define reedGlass4  4   // reed below glass 4   (D2 Output on D1 Mini from AzDelivery)
-#define reedGlass5  0   // reed below glass 5   (D3 Output on D1 Mini from AzDelivery)
-#define reedGlass6  16  // reed below glass 6   (D0 Output on D1 Mini from AzDelivery)
-#define reedGlass7  14  // reed below glass 7   (D5 Output on D1 Mini from AzDelivery)
-#define reedGlass8  12  // reed below glass 8   (D6 Output on D1 Mini from AzDelivery)
-#define reedGlass9  13  // reed below glass 9   (D7 Output on D1 Mini from AzDelivery)
-#define reedGlass10 15  // reed below glass 10  (D8 Output on D1 Mini from AzDelivery)*/
+#define RESET_PIN_LEFT  0 	// reset pin for left MCP23017 (D3 Output on D1 Mini from AzDelivery)
+#define RESET_PIN_RIGHT 2 	// reset pin for left MCP23017 (D4 Output on D1 Mini from AzDelivery)
 
 // definition of constants
 #define MCP_ADDR_LEFT   0x20  // I2C address of MCP that controls the left side of the board
@@ -52,9 +40,9 @@
 #define BRIGHTNESS      50    // set brightness of LEDs
 #define WAITTIME        500   // refresh rate for checking the glass status in ms
 // state machine constants
-#define INIT        0     // wait until all glasses are positioned
-#define LIGHTING    1     // 
-#define REMOVED     2     // 
+#define INIT        0     // stay here until all glasses are on board
+#define LIGHTING    1     // stay in this case as long as init phase is over and at least one glass is still on board
+#define REMOVED     2     // go to this case when at least one glass was removed
 
 // definition of variables
 Glass glass_left[NUMGLASSES];
@@ -64,8 +52,8 @@ Adafruit_NeoPixel strip_right = Adafruit_NeoPixel(NUMGLASSES, LEDDataRight, NEO_
 unsigned long previousMillis = 0;   // variable for counter
 int stateVar = INIT; 
 
-MCP23017 mcp_left = MCP23017(MCP_ADDR_LEFT);
-MCP23017 mcp_right = MCP23017(MCP_ADDR_RIGHT);
+MCP23017 mcp_left = MCP23017(MCP_ADDR_LEFT, RESET_PIN_LEFT);
+MCP23017 mcp_right = MCP23017(MCP_ADDR_RIGHT, RESET_PIN_RIGHT);
 
 /* initalize variables */
 void setup() {
@@ -74,6 +62,7 @@ void setup() {
   // uint8_t reedPin = 0;
   for(uint8_t i = 0; i < NUMGLASSES; i++)
   {
+    /* Port A: glasses 0 - 7, Port B: Glasses 8 - 9 */
     if (i < 8) {
       // set reed pin, port, status as false and color as "off"
       // reedPin = 0b00000000 | (1 >> (7-i));
@@ -86,14 +75,13 @@ void setup() {
       glass_left[i] = Glass((8 - i), B, false, initColor);
       glass_right[i] = Glass((8 - i), B, false, initColor);
     }
-      
-    // set all reed pins as input pins
-    pinMode(glass[i].getReedPin(), INPUT_PULLUP);
   }
   
   // initialize LEDs (also data output pin gets initialized in that step)
-  strip.begin();
-  strip.show();   // initialize all pixels to "off"
+  strip_left.begin();
+  strip_right.begin();
+  strip_left.show();  // initialize all pixels to "off"
+  strip_right.show(); // initialize all pixels to "off"
 
   // initialize MCPs
   Wire.begin(); // "start" I2C
@@ -106,7 +94,7 @@ void setup() {
   mcp_right.setPortMode(B11111111, A, INPUT_PULLUP); // set A0 - A7 as input with pullup resistor
   mcp_right.setPortMode(B00000011, B, INPUT_PULLUP); // set B0 - B1 as input with pullup resistor
   Serial.begin(115200);
-}
+} /* end of setup function */
 
 /* going into main loop */
 void loop() {
@@ -139,7 +127,7 @@ void loop() {
     default:
       break;
   }
-}
+} /* end of main loop function */
 
 /* Checks, if all glasses have been added to the board, init in mexican colors */
 void initBoard(void)
@@ -147,30 +135,53 @@ void initBoard(void)
   int count = 0;
   for(uint8_t i = 0; i < NUMGLASSES; i++)
   {
-    if (digitalRead(glass[i].reedPin) == LOW && glass[i].getGlassStatus() == false)
+    /* init left side */
+    if (mcp_left.getPin(glass_left[i].getReedPin(), glass_left[i].getPort()) == LOW && glass_left[i].getGlassStatus() == false)
     {
-      glass[i].getGlassStatus() = true;
+      glass_left[i].getGlassStatus() = true;
       if (i < 3) 
       {
-        glass[i].setColors(0, 255, 20);
+        glass_left[i].setColors(0, 255, 20);
         Serial.println("Initialize Glass in green");
       }
       else if (i > 2 && i < 6) 
       {
-        glass[i].setColors(255, 255, 255);
+        glass_left[i].setColors(255, 255, 255);
         Serial.println("Initialize Glass in white");
       }
       else if (i > 5) 
       {
-        glass[i].setColors(255, 0, 0);
+        glass_left[i].setColors(255, 0, 0);
         Serial.println("Initialize Glass in red");
       }
       turnOn(i);
     }
-    if (glass[i].getGlassStatus() == true) count += 1;
+    /* init right side */
+    if (mcp_right.getPin(glass_right[i].getReedPin(), glass_right[i].getPort()) == LOW && glass_right[i].getGlassStatus() == false)
+    {
+      glass_right[i].getGlassStatus() = true;
+      if (i < 3) 
+      {
+        glass_right[i].setColors(0, 255, 20);
+        Serial.println("Initialize Glass in green");
+      }
+      else if (i > 2 && i < 6) 
+      {
+        glass_right[i].setColors(255, 255, 255);
+        Serial.println("Initialize Glass in white");
+      }
+      else if (i > 5) 
+      {
+        glass_right[i].setColors(255, 0, 0);
+        Serial.println("Initialize Glass in red");
+      }
+      turnOn(i);
+    }
+    if (glass_left[i].getGlassStatus() == true) count += 1;
+    if (glass_right[i].getGlassStatus() == true) count += 1;
   }
   // if all glasses are on the board, go to status LIGHTING
-  if (count == NUMGLASSES) stateVar = LIGHTING;
+  if (count == 2*NUMGLASSES) stateVar = LIGHTING;
 }
 
 /* Checks, if all glasses have been removed from the board */
@@ -229,9 +240,9 @@ void turnOn(int glassIndex)
     int R = int(0.01 * k * glass[glassIndex].getColors().red);
     int G = int(0.01 * k * glass[glassIndex].getColors().green);
     int B = int(0.01 * k * glass[glassIndex].getColors().blue);
-    strip.setPixelColor(glassIndex, strip.Color(R, G, B));
-    strip.show();
-    delay(5);
+    strip_left.setPixelColor(glassIndex, strip_left.Color(R, G, B));
+    strip_left.show();
+    delay(5); // control duration of fading effect
   }
 }
 
@@ -243,8 +254,8 @@ void turnOff(int glassIndex)
     int R = int(0.01 * k * glass[glassIndex].getColors().red);
     int G = int(0.01 * k * glass[glassIndex].getColors().green);
     int B = int(0.01 * k * glass[glassIndex].getColors().blue);
-    strip.setPixelColor(glassIndex, strip.Color(R, G, B));
-    strip.show();
-    delay(5);
+    strip_left.setPixelColor(glassIndex, strip_left.Color(R, G, B));
+    strip_left.show();
+    delay(5); // control duration of fading effect
   }
 }
